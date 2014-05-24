8f600f4918fa71eb3c99c5f23197e4784dac9567
y-ya <y_ya8511@hotmail.co.jp>
2014-05-24 17:03:26 +0900

    change dir name and add cardCreate sample

---
+++ simple_card/Assets/Script/Card.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+	
+	private int    __number;
+	private string __mark;
+	public int    number{ get; set; }
+	public string mark  { get; set; }
+}
---
+++ simple_card/Assets/Script/CreateCard.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*public class CreateCard : MonoBehaviour {
+}*/
+public class CreateCard {
+
+	private readonly string   resource_path = "card/{0}";
+	private readonly string[] card_type     = {"c","s","d","h"};
+
+	/*public Texture2D findCardOfTexture2D( int number = 0 , string mark = null ){
+		if(string.IsNullOrEmpty(mark))
+			mark = card_type[Random.Range(0,4)];
+		if(number <= 0)
+			number = Random.Range(1,14);
+		string file_name = makeFileString (number, mark);
+		return Resources.Load<Texture2D> ( file_name );
+	}*/
+	public GameObject findCardOfGameObject( int number = 0 , string mark = null ){
+		if(string.IsNullOrEmpty(mark))
+			mark = card_type[Random.Range(0,4)];
+		if(number <= 0)
+			number = Random.Range(1,14);
+		string file_name = makeFileString (number, mark);
+
+		GameObject obj = Resources.Load<GameObject> ( "card" );
+		Card card = obj.GetComponent<Card> ();
+		card.number = number;
+		card.mark = mark;
+		obj.renderer.material.mainTexture = Resources.Load<Texture2D> ( file_name );
+		return obj;
+	}
+
+	private string makeFileString( int number, string mark ){
+		return string.Format (resource_path, mark + number.ToString ("00"));
+	}
+}
