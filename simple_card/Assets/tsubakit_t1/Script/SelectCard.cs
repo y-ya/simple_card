6c8626705448eb58cd76afd7525fe33b42c990f4
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-05-24 16:05:57 +0900

    オブジェクトの選択

---
+++ simple_card/Assets/tsubakit_t1/Script/SelectCard.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectCard : MonoBehaviour
+{
+	public delegate void TapObject (GameObject forcus);
+
+
+	public event TapObject forcusObjectE = null;
+	public event TapObject releaseObjectE = null;
+	public GameObject forcusObject { get; private set; }
+	public bool isSelected {
+		get {
+			return forcusObject != null;
+		}
+	}
+
+	protected Camera _mainCamera; 
+
+	void Start ()
+	{
+		_mainCamera = Camera.main;
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+
+			//var ray = _mainCamera.ScreenPointToRay (Input.mousePosition);
+			var tapPosition = _mainCamera.ScreenToWorldPoint (Input.mousePosition + Vector3.forward * -_mainCamera.transform.position.z);
+
+			var collider = Physics2D.OverlapCircle (tapPosition, 0.1f);
+			if (collider != null) {
+				forcusObject = collider.gameObject;
+				if (forcusObjectE != null) {
+					forcusObjectE (forcusObject);
+				}
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			if (forcusObject != null && releaseObjectE != null) {
+				releaseObjectE (forcusObject);
+			}
+
+			forcusObject = null;
+		}
+	}
+
+	void OnDestroy ()
+	{
+		forcusObjectE = null;
+	}
+
+
+}
---
+++ simple_card/Assets/tsubakit_t1/Script/SelectCardTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectCardTest : MonoBehaviour {
+
+	SelectCard selectCard;
+
+	void Awake ()
+	{
+		selectCard = GetComponent<SelectCard>();
+
+		selectCard.forcusObjectE += (forcus) => 
+		{
+			Debug.Log("forcus object" + forcus.name);
+		};
+		selectCard.releaseObjectE += (forcus) => 
+		{
+			Debug.Log("release object" + forcus.name);
+		};
+	}
+	
+	void OnGUI ()
+	{
+		if (selectCard.forcusObject != null) {
+			GUILayout.Label (selectCard.forcusObject.name);
+		}
+	}
+}
