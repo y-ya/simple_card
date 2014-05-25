206c5e3bf215610e7c45b29b30caccb12dfc3528
吉田 俊介 <shunsoft@gmail.com>
2014-05-25 12:45:10 +0900

    [Sound] add Music.cs

--- simple_card/Assets/shunsoft/Script/Player.cs
+++ simple_card/Assets/shunsoft/Script/Player.cs
@@ -3,10 +3,35 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	void Update () {
+		// music sync UI
+		guiText.text = "Just: " + Music.Just.ToString();
+		if( Music.IsJustChangedBar() ) {
+			guiText.material.color = Music.Just.bar % 2 == 0 ? Color.white : Color.yellow;
+		}
+		int mtBeat = Music.mtBeat;
+		guiText.transform.position = new Vector3(0.5f, 0.5f + 0.02f * (float)(mtBeat - Music.MusicalTime % mtBeat)/mtBeat, 0);
+
+		// easy quantize play
+		if( Input.GetMouseButtonDown(0) ) {
+			Music.QuantizePlay( new Music.SoundCue(audio) );
+			
+		}
+
+		// you can pause
+		if( Input.GetMouseButtonDown(1) ) {
+			if( Music.IsPlaying() ) {
+				Music.Pause();
+			} else {
+				Music.Resume();
+			}
+		}
+
+#if !ADX
 		if( Input.GetKeyDown(KeyCode.A) == true ) {
 			// Torigger
-			Debug.Log( "A!" );
+			Music.CurrentSource.source.pitch = Music.CurrentSource.source.pitch > 1.0f ? 1.0f : 2.0f;
 		}
+#endif
 	}
 
 	void OnGUI() {
