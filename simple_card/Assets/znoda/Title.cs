b079e692be3a60893ea0e6c7155e45aad2896ddd
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-05-25 17:22:46 +0900

    タイトルから起動するように変更

--- simple_card/Assets/znoda/Title.cs
+++ simple_card/Assets/znoda/Title.cs
@@ -3,20 +3,19 @@ using System.Collections;
 
 public class Title : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnGUI() {
 		if (GUI.Button (new Rect (Screen.width / 2 - 100, 200, 200, 30), "Game Start"))
 		{
-			Application.LoadLevel("Demo");
+			enabled = false;
+			var fadeCamera = FadeCamera.Instance;
+			fadeCamera.UpdateMaskTexture(Resources.Load<Texture>("test"));
+			fadeCamera.FadeOut( 3,  ()=>
+			{
+				Application.LoadLevel("Demo");
+				fadeCamera.UpdateMaskTexture(Resources.Load<Texture>("test2"));
+				fadeCamera.FadeIn(0.3f, null);
+			});
+
 		}
 	}
 }
