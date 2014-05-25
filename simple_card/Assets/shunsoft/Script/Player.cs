da9608dadec33767917d3be2517f89ab158bccd4
吉田 俊介 <shunsoft@gmail.com>
2014-05-25 10:40:30 +0900

    [Sound]Tag bug fixed., add button play.

--- simple_card/Assets/shunsoft/Script/Player.cs
+++ simple_card/Assets/shunsoft/Script/Player.cs
@@ -6,9 +6,18 @@ public class Player : MonoBehaviour {
 		if( Input.GetKeyDown(KeyCode.A) == true ) {
 			// Torigger
 			Debug.Log( "A!" );
-			//Singleton<SoundPlayer>.instance.playSE("se001");
+		}
+	}
+
+	void OnGUI() {
+		if( GUI.Button(new Rect(10, 20, 120, 80), "BGMPlay") ) {
+			SoundManager.Instance.PlayBGM(0);
+		}
+		if( GUI.Button(new Rect(10+10+120, 20, 120, 80), "BGMStop") ) {
+			SoundManager.Instance.StopBGM();
+		}
+		if( GUI.Button(new Rect(10+(10+120)*2, 20, 120, 80), "SEPlay") ) {
 			SoundManager.Instance.PlaySE(0);
-			//SoundManager.Instance.PlayBGM(0);
 		}
 	}
 }
