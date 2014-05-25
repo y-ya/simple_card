0c33ed416c79faaa0e464af625817221f3a905de
NodaSatoru <i.lovethemachine@gmail.com>
2014-05-25 18:59:54 +0900

    タイトルのシーンを変更

---
+++ simple_card/Assets/znoda/TitleLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("Demo");
+		}
+	}
+
+//	void OnGUI() {
+//		if (GUI.Button (new Rect (Screen.width / 2 - 100, 200, 200, 30), "Game Start"))
+//		{
+//			Application.LoadLevel("Demo");
+//		}
+//	}
+}
